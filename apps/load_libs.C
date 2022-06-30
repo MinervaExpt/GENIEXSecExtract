3516417145327407e5f431783de429ef7a4f4ce3
Ben Messerly <bamesserly@gmail.com>
2022-06-30 15:26:29 -0500

    Modernize load_libs.
    For those not making closure executables that link against
    GXSE, you can run your closure scripts from the command
    line as usual like root load_libs.C+ my_closure_script.C+
    
    Just updated SO locations to reflect default install of
    GXSE and MAT.

--- apps/load_libs.C
+++ apps/load_libs.C
@@ -1,23 +1,35 @@
+#include "TSystem.h"
+#include "TInterpreter.h"
+#include "TROOT.h"
+#include <iostream>
+#include <string>
+
 void load_libs()
 {
   gSystem->SetAclicMode(TSystem::kDebug);
 
+  // hide override warnings
+  TString makeSharedLib(gSystem->GetMakeSharedLib());
+  makeSharedLib.ReplaceAll("-Woverloaded-virtual", "-Wno-overloaded-virtual");
+  gSystem->SetMakeSharedLib(makeSharedLib);
+
   //add GENIEXSecExtact stuff
   {
-  gInterpreter->AddIncludePath( gSystem->ExpandPathName("$GENIEXSECEXTRACTROOT") );
-  string newpath = string(gROOT->GetMacroPath()) + ":" + string(gSystem->ExpandPathName("$GENIEXSECEXTRACTROOT")) + "/GENIEXSecExtract";
-  gROOT->SetMacroPath( newpath.c_str() );
-  gSystem->Load( gSystem->ExpandPathName("$GENIEXSECEXTRACTROOT/$CMTCONFIG/libGENIEXSecExtract.so") );
+    gInterpreter->AddIncludePath( gSystem->ExpandPathName("$GENIEXSECEXTRACTROOT") );
+    std::string newpath = std::string(gROOT->GetMacroPath()) + ":" + std::string(gSystem->ExpandPathName("$GENIEXSECEXTRACTROOT"));
+    gROOT->SetMacroPath( newpath.c_str() );
+    gSystem->Load( gSystem->ExpandPathName("$GENIEXSECEXTRACTROOT/libGENIEXSecExtract.so") );
   }
 
-  //add PlotUtils stuff
+  //add MAT/PlotUtils stuff
   {
     gInterpreter->AddIncludePath( gSystem->ExpandPathName("$PLOTUTILSROOT") );
-    string newpath = string(gROOT->GetMacroPath()) + ":" + string(gSystem->ExpandPathName("$PLOTUTILSROOT")) + "/PlotUtils";
+    std::string newpath = std::string(gROOT->GetMacroPath()) + ":" + std::string(gSystem->ExpandPathName("$PLOTUTILSROOT"));
     gROOT->SetMacroPath( newpath.c_str() );
-    gSystem->Load( gSystem->ExpandPathName("$PLOTUTILSROOT/$CMTCONFIG/libplotutils.so") );
+    gSystem->Load( gSystem->ExpandPathName("$PLOTUTILSROOT/libMAT.so") );
   }
 
-  gSystem->Load( "libCintex.so" );  // needed to process the dictionaries for the objects
-  Cintex::Enable();
+  // for ROOT5
+  //gSystem->Load( "libCintex.so" );  // needed to process the dictionaries for the objects
+  //Cintex::Enable();
 }
