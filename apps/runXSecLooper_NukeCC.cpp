9902f983b38f015ae500e677a972b9c1de3eca55
Amy Filkins <afilkins@minervagpvm04.fnal.gov>
2022-09-21 19:17:38 -0500

    Updates used for 1D nu DIS closure tests, shoud fix some segfaults caused by changes to XSecLooper

--- apps/runXSecLooper_NukeCC.cpp
+++ apps/runXSecLooper_NukeCC.cpp
@@ -15,13 +15,16 @@ namespace {
     const double TRACKER_ZMIN = 6117;
     const double TRACKER_ZMAX = 8193;
 }
+bool debug=false;
+bool anyTracker=true;
 
 double GetNormValue( int targetID, int targetZ, int targetNucleon = 0 )
 {
-    
-    double trackerAtomsC = TargetUtils::Get().GetTrackerElementNAtoms( 6, 104., 1 );
-    
-    cout<<trackerAtomsC<<"  "<<2.22311e+27 * 104.<<endl;
+
+  //double trackerAtomsC = TargetUtils::Get().GetTrackerElementNAtoms( 6, 5990., 8340., 1 ); //z 5990-8340 = [27,78]=52 modules
+  double trackerAtomsC = TargetUtils::Get().GetTrackerElementNAtoms( 6, 106., 1 ); //[27,79] is 53 modules, not 52.
+  cout<<"trackerAtomsC: " <<trackerAtomsC<<endl;
+
     
     
     //    const double trackerAtomsC = 2.22311e+27 * 92.;
@@ -31,12 +34,12 @@ double GetNormValue( int targetID, int targetZ, int targetNucleon = 0 )
     if ( targetNucleon == 0 ){
         
         if(targetID < 10 ){
-            
             passiveNucleons = TargetUtils::Get().GetPassiveTargetNNucleons(targetID,targetZ, true);
-            
+	    cout<<"target "<<targetID<<" N nucleons: "<<passiveNucleons<<endl;
         }
         if(targetID > 10){
             passiveNucleons = TargetUtils::Get().GetTrackerNNucleons( 12., true, 850. );
+	    if(anyTracker) passiveNucleons*=9;
         }
         
     }
@@ -74,14 +77,16 @@ public:
     m_isDIS( isDIS),
     m_isinHex( InHex),
     m_truedisttoDiv(PassTrueDistToDivisionCut ){};
-    
+  
+
     double Qsquared(ChainWrapper& chw, int entry){
         const int PROTON_PDG = 2212;
         const int NEUTRON_PDG = 2112;
         const double M_neutron = 939.56;
         const double M_proton  = 938.27;
         const double M_nucleon  = ( 1.5*M_neutron + M_proton ) / 2.5;
-        
+        const double M_muon2= pow(105.66, 2);
+
         const double PI = atan(1.0)*4;
         const double deg_to_rad = PI / 180.0;
         const double rad_to_deg = 1. / deg_to_rad;
@@ -93,8 +98,13 @@ public:
         double muon_theta = (double)chw.GetValue("truth_muon_theta",entry);
         
         double neutrino_E = (double)chw.GetValue("mc_incomingE",entry);
-        double Q2 = 4*neutrino_E*muon_E*pow( sin( muon_theta /2 ), 2. )/(1000*1000);
+        double Q2_approxed = 4*neutrino_E*muon_E*pow( sin( muon_theta /2 ), 2. )/(1000*1000);
         //        cout<<"The Q2 is "<<Q2<<endl;
+	double pmu2=pow(muon_E,2) - M_muon2;
+	double pmu= (pmu2>0)? sqrt(pmu2):0;
+	double Q2= 2* neutrino_E* (muon_E - pmu*cos(muon_theta)  ) - M_muon2;
+	Q2/= 1000*1000;
+
         return Q2;
     }
     
@@ -168,9 +178,8 @@ public:
     
     bool PassTrueDistToDivisionCut( ChainWrapper& chw, int entry, int targetID, int num_targetZ, double xySep /* = 25. */ )
     {
-        
         double true_target_dist_to_division = (double)chw.GetValue("truth_target_dist_to_division",entry);
-        
+	
         int true_targetID = (int)chw.GetValue("truth_targetID",entry);
         int true_targetZ  = (int)chw.GetValue("truth_targetZ",entry);
         int true_module   = (int)chw.GetValue("truth_vtx_module",entry);
@@ -179,22 +188,25 @@ public:
         if( targetID < 10 && true_targetID != targetID) return false;
         //        cout<<true_targetZ<< "  target ID "<<num_targetZ<<endl;
         if( targetID < 10 && true_targetZ != num_targetZ) return false;
-        if( targetID == 14 && ( true_module < 27 || true_module > 32)) return false;
-        if( targetID == 24 && ( true_module < 33 || true_module > 38)) return false;
-        if( targetID == 34 && ( true_module < 39 || true_module > 44)) return false;
-        if( targetID == 44 && ( true_module < 45 || true_module > 50)) return false;
-        if( targetID == 54 && ( true_module < 51 || true_module > 56)) return false;
-        if( targetID == 64 && ( true_module < 57 || true_module > 62)) return false;
-        if( targetID == 74 && ( true_module < 63 || true_module > 68)) return false;
-        if( targetID == 84 && ( true_module < 69 || true_module > 74)) return false;
-        if( targetID == 94 && ( true_module < 75 || true_module > 81)) return false;
-        
+	if(targetID >10 && anyTracker) {
+	  if( true_module < 27 || true_module > 80 ) return false;
+	}
+	else{
+	  if( targetID == 14 && ( true_module < 27 || true_module > 32)) return false;
+	  if( targetID == 24 && ( true_module < 33 || true_module > 38)) return false;
+	  if( targetID == 34 && ( true_module < 39 || true_module > 44)) return false;
+	  if( targetID == 44 && ( true_module < 45 || true_module > 50)) return false;
+	  if( targetID == 54 && ( true_module < 51 || true_module > 56)) return false;
+	  if( targetID == 64 && ( true_module < 57 || true_module > 62)) return false;
+	  if( targetID == 74 && ( true_module < 63 || true_module > 68)) return false;
+	  if( targetID == 84 && ( true_module < 69 || true_module > 74)) return false;
+	  if( targetID == 94 && ( true_module < 75 || true_module > 80)) return false;//true_module > 81)) return false;
+	}
         //            only relevant for passive targets 1235
         if( 0 < true_targetID && true_targetID < 10 && 4 != true_targetID)
             return ( xySep < true_target_dist_to_division );
         //        cout<<"fiducial event "<<targetID<<endl;
-        
-        
+
         return true;
     }
     
@@ -203,7 +215,6 @@ public:
     //! Return QE-like signal
     bool isDIS( ChainWrapper& chw, int entry ) {
         //        cout<<"I'm calling the signal definition "<<endl;
-        
         const double PI = atan(1.0)*4;
         const double deg_to_rad = PI / 180.0;
         const double rad_to_deg = 1. / deg_to_rad;
@@ -214,7 +225,6 @@ public:
         int is_neutrino = (int)chw.GetValue("mc_incoming",entry);
         //        cout<<"am I a neutrino? "<<is_neutrino<<endl;
         if(is_neutrino!=14) return false;
-        
         //right interaction type
         int is_CC     = (int)chw.GetValue("mc_current",entry);
         //        cout<<"am I CC? "<<is_CC<<endl;
@@ -223,7 +233,8 @@ public:
         //right genie kindmaitcs
         int genie_dis = (int)chw.GetValue("mc_intType",entry);
         //        cout<<"am I a GENIE DIS Event? "<<genie_dis<<endl;
-        if(genie_dis==1 || genie_dis==2) return false;
+	//No longer included in signal definition
+        //if(genie_dis==1 || genie_dis==2) return false;
         
         
         double muon_E     = (double)chw.GetValue("mc_primFSLepton",entry,3);
@@ -234,10 +245,8 @@ public:
         if( muon_theta*rad_to_deg >= 17.0 || muon_theta*rad_to_deg < 0.0 ) return false;
         
         
-        
         double Q2 = Qsquared(chw, entry);
         double W  = W_had(chw, entry);
-        
         if(Q2 < 1.0) return false;
         if(W < 2.0)  return false;
         
@@ -253,7 +262,6 @@ public:
         if(!isDIS(chw,entry)) return false;
         if(!InHex(chw,entry,850.0)) return false;
         if(!PassTrueDistToDivisionCut( chw, entry, m_target, m_nucleus, 25.0)) return false;
-        
         //this stuff is nice to have for event by event checking... 
         int run = (int)chw.GetValue("mc_run",entry);
         int subrun = (int)chw.GetValue("mc_subrun",entry);
@@ -293,19 +301,35 @@ public:
     
 };
 
+//========================
+///XSecLooper for NukeCC
+//=======================
+class NukeCC_XSecLooper : public XSecLooper
+{
+  public:
+    NukeCC_XSecLooper(const char* inputFileGlob) :  XSecLooper(inputFileGlob){};
+
+  //override this function thats cutting out target events, fiducial is defined in event selection.
+  bool isFiducial(ChainWrapper& chw, int entry){
+    //cout<<"runXSecLooper:isFiducial"<<endl;
+    return true;
+  }
+
+  
+};
+
 //======================
 //! main:
 //======================
 //void runXSecLooper_NukeCC( string& target_type )
 void runXSecLooper_NukeCC( )
 {
-    
+    string playlist="minervame1D";
     //! get the ntuple directory names
     string directory   = "genie_xsection_ntuples";
     string output_name = "genie_xsections";
     
 //    output_name += "_" + target_type;
-    
     
     output_name += "_histos.root";
     
@@ -313,12 +337,16 @@ void runXSecLooper_NukeCC( )
     
     //! Create the XSecLooper and tell it the input files
     
-//    XSecLooper loop("/minerva/data/users/norrick/NukeTuples/v1_WithTargetCode_MLNX/NukeTruth_AnaTuple_Tgt*minervame1D*.root");
-    XSecLooper loop("/minerva/data/users/norrick/NukeTuples/v1_WithTargetCode_MLNX/NukeTruth_AnaTuple_minervame1A_v1_WithTargetCode_MLNX.root");
-    
+
+    //"/minerva/data/users/norrick/NukeTuples/v1_WithTargetCode_MLNX/NukeTruth_AnaTuple_minervame1A_v1_WithTargetCode_MLNX.root");
+    //    XSecLooper loop("/minerva/data/users/norrick/NukeTuples/v1_WithTargetCode_MLNX/NukeTruth_AnaTuple_Tgt*minervame1D*.root");
+    NukeCC_XSecLooper loop(Form("%s/%s/NukeCC_MC_AnaTuple_%s_*_%s.root", getenv("TUPLES"), getenv("NUKECC_V"), playlist.c_str(), getenv("NUKECC_V")) );
+
+    //This is probably wrong. 
     //! Tell the XSecLooper which neutrino type we're considering (mandatory)
     loop.setNuPDG(14);
-    
+    //loop.setFiducial(true, 5990, 8340);//not the order of right arguments for this function anymore (theres no bool)
+    loop.setFiducial(5980,8375);//modules [27,79]
     //! Setting the number of Universes in the GENIE error band (default 100, put 0 if you do not want to include the universes)
     loop.setNumUniv(0); //Tammy used 100
     
@@ -335,28 +363,28 @@ void runXSecLooper_NukeCC( )
     
     vector<XSec::EVariable> vars;
     vars.push_back( XSec::kENu );
-//vars.push_back( XSec::kEHad );
-//    vars.push_back( XSec::kELep );
+    //vars.push_back( XSec::kEHad );
+    //vars.push_back( XSec::kELep );
     vars.push_back( XSec::kxExp );
-//    vars.push_back( XSec::ky );
-////    //    //For Debugging
-//    vars.push_back( XSec::kExpW );
-//    vars.push_back( XSec::kExpQ2 );
-//    vars.push_back( XSec::kThetaLep );
+    //    vars.push_back( XSec::ky );
+    //For Debugging
+    //vars.push_back( XSec::kExpW );
+    //vars.push_back( XSec::kExpQ2 );
+    //vars.push_back( XSec::kThetaLep );
     
     vector<int> targetZs;
-     targetZs.push_back(6);
-     targetZs.push_back(26);
+    targetZs.push_back(6);
+    targetZs.push_back(26);
     targetZs.push_back(82);
-    
+    //targetZs.push_back(1);
     vector<int> targetIDs;
-      targetIDs.push_back(1);
+    //targetIDs.push_back(1);
 //    targetIDs.push_back(2);
-//    targetIDs.push_back(3);
+    targetIDs.push_back(3);
 //    targetIDs.push_back(4);
 //    targetIDs.push_back(5);
     targetIDs.push_back(14);
-    targetIDs.push_back(24);
+    //targetIDs.push_back(24);
 /*   targetIDs.push_back(34);
     targetIDs.push_back(44);
     targetIDs.push_back(54);
@@ -364,7 +392,7 @@ void runXSecLooper_NukeCC( )
     targetIDs.push_back(74);
     targetIDs.push_back(84);
     targetIDs.push_back(94);*/
-    
+
     for(int i=0; i <targetIDs.size(); ++i){
         
         for(int j=0; j <targetZs.size(); ++j){
@@ -422,7 +450,6 @@ void runXSecLooper_NukeCC( )
                 if(num_targetZ==82 && num_target< 10) nuc_name = "lead";
                 if(num_targetZ==82 && num_target> 10) nuc_name = "tracker";
                 
-                
                 char* name = Form( "%s_%d_%s_std_dis", nuc_name.c_str(), num_target, varName.c_str());
                 cout<<"making cross section for "<<num_target<<"  "<<num_targetZ<<endl;
                 NukeCCXSec* xsec = new NukeCCXSec( name, num_target, num_targetZ, true, true, true );
@@ -434,15 +461,15 @@ void runXSecLooper_NukeCC( )
                 //! set cross section data
                 xsec->setBinEdges(num_bins,&bins.front());
                 xsec->setNormalizationValue( GetNormValue( num_target, num_targetZ ) );
-                
                 xsec->setVariable(*var);
                 if(*var !=XSec::kENu) xsec->setIsFluxIntegrated(true);
                 else  xsec->setIsFluxIntegrated(false);
                 
-                xsec->setFluxIntLimits(0.,120.);
+                xsec->setFluxIntLimits(0.,120.);//(0,50);//
                 xsec->setUniverses(0);
-                //                xsec->setNormalizationType(XSec::kPerNucleon);
+		//xsec->setNormalizationType(XSec::kPerNucleon);
                 xsec->setNormalizationType(XSec::kSelfNorm);
+		xsec->setDimension(1);//Not sure why I suddenly need this? weird...
                 //! add cross section
                 loop.addXSec(xsec);
                 
@@ -452,10 +479,9 @@ void runXSecLooper_NukeCC( )
     
     //! run
     loop.runLoop();
-    
     //! get the output histograms and save them to file
-    string fname = "/minerva/data/users/${USER}/NukeFiles/NukeCC_xsec_minervame1A_Sept4.root";
-//     string fname = "/minerva/data/users/${USER}/NukeFiles/NukeCC_xsec_CRAP.root";
+    string fname = "/minerva/data/users/${USER}/NukeFiles/NukeCC_xsec_minervame1D_targets_NuEOn.root";
+
     TFile fout(fname.c_str(), "RECREATE");
     
     for(unsigned int i = 0; i < loop.getXSecs().size(); i++) {
@@ -491,7 +517,7 @@ int main( int argc, char *argv[] )
     
         
         cout << "running genie xsection extraction" << endl;
-        
+	TH1::AddDirectory(false);//maybe I need this???
 //        runXSecLooper_NukeCC( target_type );
             runXSecLooper_NukeCC();
         
